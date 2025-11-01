16fecc2cbc6c0f23cd0f761240019ceface40480
evana-gray <evanagray@gmail.com>
2025-11-01 17:56:17 -0400

    20251101_7

--- proj2_app.R
+++ proj2_app.R
@@ -18,7 +18,7 @@ nfl_pbp <- read_csv("NFL Play by Play 2009-2018 (v5).csv")
 
 # initial subset and clean
 nfl_pbp <- nfl_pbp |> 
-  filter(penalty_yards > 0 & play_type %in% c('run','pass') & length(penalty_player_id) > 1) |>
+  filter(penalty_yards > 0 & play_type %in% c('run','pass') & length(penalty_player_id) >= 2) |>
   select(play_id, game_id, game_date, home_team, play_type, game_half, qtr,
          penalty_team, penalty_player_id, penalty_yards, penalty_type) |>
   rename("gsis_id" = penalty_player_id,
