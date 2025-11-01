3bdb6d12c314d6d28342dbf5726291f647cf1065
evana-gray <evanagray@gmail.com>
2025-11-01 11:49:03 -0400

    20251101_2

--- proj2_app.R
+++ proj2_app.R
@@ -10,7 +10,14 @@ library(shinyalert)
 # then, use nflreadr::load_players() to gather all player info (position, date of birth, etc) to use for left join
 # https://nflreadr.nflverse.com/reference/load_players.html
 
-#nfl_pbp <- read_csv("NFL Play by Play 2009-2018 (v5).csv") 
+nfl_pbp <- read_csv("NFL Play by Play 2009-2018 (v5).csv") 
+
+# testing data subset to reduce size. 
+# original file is listed in gitignore
+nfl_pbp <- nfl_pbp |> 
+  filter(penalty_yards > 0) |>
+  select(play_id, game_id,penalty_team, penalty_player_id, penalty_yards, penalty_type)
+  
 players <- load_players()
 
 #join data on gsis_id - an established key specific to each NFL player 
